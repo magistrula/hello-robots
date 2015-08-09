4beb50b0d11fc95057e9b982f158e0fa058b767f
Anna Andresian <anna1978@gmail.com>
2015-08-09 10:58:42 -0400

    Add charge, dance, panic functions to activate buttons

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,45 @@
-var charge = function () {
+var DANCE_MESSAGES = [
+  'Everybody dance now!',
+  'Can\'t touch this!',
+  'Livin\' la vida loca!',
+  'You danced me too many times.',
+  'Ughhhhhhhhhhhh...'
+];
+var TIRED_THRESHOLD = 3;
+var PANIC_THRESHOLD = DANCE_MESSAGES.length;
+var danceCounter = 0;
 
+var charge = function () {
+  setMessage('Fueling...');
+  setImageBackground('green');
+  danceCounter = 0;
 }
 
 var dance = function () {
+  if (danceCounter >= PANIC_THRESHOLD) {
+    panic();
+    return;
+  }
 
+  if (danceCounter >= TIRED_THRESHOLD) {
+    setImageBackground('gray');
+  } else {
+    setImageBackground('yellow');
+  }
+
+  setMessage(DANCE_MESSAGES[danceCounter]);
+  danceCounter++;
 }
 
 var panic = function () {
+  setMessage('AUGHHHHHHH!!!!');
+  setImageBackground('red');
+}
 
+var setMessage = function (text) {
+  document.getElementById('message').innerHTML = text;
+}
+
+var setImageBackground = function (color) {
+  document.getElementById('image-container').style.backgroundColor = color;
 }
