7d9151fa938b8210edae5b058fbd496ab22ec1c2
Anna Andresian <anna1978@gmail.com>
2015-08-09 12:27:31 -0400

    Get list of robot images from server

--- app/js/script.js
+++ app/js/script.js
@@ -1,57 +1,71 @@
-var DEFAULT_MESSAGE = 'Hello, Robots!';
-var DANCE_DATA = [
-  { text: 'Everybody dance now!', color: '#FFFF66' },
-  { text: 'Can\'t touch this!', color: '#FFFF66' },
-  { text: 'Livin\' la vida loca!', color: '#FFFF66' },
-  { text: 'You danced me too many times.', color: '#E6E6E6' },
-  { text: 'Ughhhhhhhhhhhh...', color: '#FFCC99' }
-];
-var PANIC_THRESHOLD = DANCE_DATA.length;
-var ROBOTS = ['robot0','robot1','robot2','robot3','robot4','robot5'];
-var danceCounter = 0;
-var robotCounter = 0;
+$.get('/robots', function (robots) {
+  init(window, robots);
+});
 
-var charge = function () {
-  setMessage('Fueling...');
-  setImageBackground('#5CD65C');
-  danceCounter = 0;
-}
+function init(target, robots) {
 
-var dance = function () {
-  var danceDatum;
+  var DEFAULT_MESSAGE = 'Hello, Robots!';
+  var DANCE_DATA = [
+    { text: 'Everybody dance now!', color: '#FFFF66' },
+    { text: 'Can\'t touch this!', color: '#FFFF66' },
+    { text: 'Livin\' la vida loca!', color: '#FFFF66' },
+    { text: 'You danced me too many times.', color: '#E6E6E6' },
+    { text: 'Ughhhhhhhhhhhh...', color: '#FFCC99' }
+  ];
+  var PANIC_THRESHOLD = DANCE_DATA.length;
+  var ROBOTS = robots;
 
-  if (danceCounter >= PANIC_THRESHOLD) {
-    panic();
-  } else {
-    danceDatum = DANCE_DATA[danceCounter];
-    setImageBackground(danceDatum.color);
-    setMessage(danceDatum.text);
-    danceCounter++;
+  var danceCounter = 0;
+  var robotCounter = 0;
+
+  var charge = function () {
+    setMessage('Fueling...');
+    setImageBackground('#5CD65C');
+    danceCounter = 0;
   }
-}
 
-var panic = function () {
-  setMessage('AUGHHHHHHH!!!!');
-  setImageBackground('#FF4D4D');
-}
+  var dance = function () {
+    var danceDatum;
 
-var changeRobot = function () {
-  robotCounter++;
-  var robot = ROBOTS[robotCounter % ROBOTS.length];
-  document.getElementById('robot-image').src = 'img/' + robot + '.png';
-  resetRobot();
-}
+    if (danceCounter >= PANIC_THRESHOLD) {
+      panic();
+    } else {
+      danceDatum = DANCE_DATA[danceCounter];
+      setImageBackground(danceDatum.color);
+      setMessage(danceDatum.text);
+      danceCounter++;
+    }
+  }
 
-var resetRobot = function () {
-  setImageBackground('transparent');
-  setMessage(DEFAULT_MESSAGE);
-  danceCounter = 0;
-}
+  var panic = function () {
+    setMessage('AUGHHHHHHH!!!!');
+    setImageBackground('#FF4D4D');
+  }
 
-var setMessage = function (text) {
-  document.getElementById('message').innerHTML = text;
-}
+  var changeRobot = function () {
+    robotCounter++;
+    var robot = ROBOTS[robotCounter % ROBOTS.length];
+    document.getElementById('robot-image').src = 'img/' + robot;
+    resetRobot();
+  }
 
-var setImageBackground = function (color) {
-  document.getElementById('image-container').style.backgroundColor = color;
+  var resetRobot = function () {
+    setImageBackground('transparent');
+    setMessage(DEFAULT_MESSAGE);
+    danceCounter = 0;
+  }
+
+  var setMessage = function (text) {
+    document.getElementById('message').innerHTML = text;
+  }
+
+  var setImageBackground = function (color) {
+    document.getElementById('image-container').style.backgroundColor = color;
+  }
+
+  target.charge = charge;
+  target.dance = dance;
+  target.panic = panic;
+  target.changeRobot = changeRobot;
+
 }
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express'),
+    fs = require('fs'),
     app = express();
 
 app.use(express.static('app'));
+
+app.get('/robots', function (request, response, next) {
+  fs.readdir('app/img', function (err, files) {
+    response.send(files);
+  });
+});
 
 var server = app.listen(3000, function () {
   var host = server.address().address,
